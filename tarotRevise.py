efc05130a4ac5c135f440e3af2b7d18cab3923b6
CaptainStinkRat <116904070+CaptainStinkRat@users.noreply.github.com>
2022-11-16 09:42:54 -0600

    Update tarotRevise.py

--- tarotRevise.py
+++ tarotRevise.py
@@ -1,5 +1,6 @@
 import random
 from random import randint
+from PIL import Image, ImageFont, ImageDraw
 
 class card:
     def __init__(self,meaning,cardFace,rev):
@@ -31,19 +32,72 @@ allCardFaces = [('fool'),('magician'),('highpriestess'),('empress'),('emperor'),
 cardMeaningNorm = allCardMeaning[::2]
 cardMeaningRev = allCardMeaning[1::2]
 
-cardPullFacing = randint(0,1)
+cardPullFacing1 = randint(0,1)
 
-if cardPullFacing == 0:
+if cardPullFacing1 == 0:
     cardMeaningNormNum = randint(0,21)
     cardMeaningNormPull = cardMeaningNorm[cardMeaningNormNum]
     cardMeaningNormFace = allCardFaces[cardMeaningNormNum]
     card1 = card(cardMeaningNormPull,cardMeaningNormFace,False)
+    cardImg1 = Image.open("tarot-{cardMeaningNormFace}.jpg")
     print(card1)
-elif cardPullFacing == 1:
+elif cardPullFacing1 == 1:
     cardMeaningRevNum = randint(0,21)
     cardMeaningRevPull = cardMeaningRev[cardMeaningRevNum]
     cardMeaningRevFace = allCardFaces[cardMeaningRevNum]
     card1 = card(cardMeaningRevPull,cardMeaningRevFace,True)
+    cardImg1 = Image.open("tarot-{cardMeaningRevFace}.jpg")
+    cardImg1_Rev = cardImg1.rotate(180)
+    cardImg1 = cardImg1_Rev
     print(card1)
 else:
     print('error')
+
+cardPullFacing2 = randint(0,1)
+
+if cardPullFacing2 == 0:
+    cardMeaningNormNum = randint(0,21)
+    cardMeaningNormPull = cardMeaningNorm[cardMeaningNormNum]
+    cardMeaningNormFace = allCardFaces[cardMeaningNormNum]
+    card2 = card(cardMeaningNormPull,cardMeaningNormFace,False)
+    cardImg2 = Image.open("tarot-{cardMeaningNormFace}.jpg")
+    print(card2)
+elif cardPullFacing2 == 1:
+    cardMeaningRevNum = randint(0,21)
+    cardMeaningRevPull = cardMeaningRev[cardMeaningRevNum]
+    cardMeaningRevFace = allCardFaces[cardMeaningRevNum]
+    card2 = card(cardMeaningRevPull,cardMeaningRevFace,True)
+    cardImg2 = Image.open("tarot-{cardMeaningRevFace}.jpg")
+    cardImg2_Rev = cardImg2.rotate(180)
+    cardImg2 = cardImg2_Rev
+    print(card2)
+else:
+    print('error')
+
+cardPullFacing3 = randint(0,1)
+
+if cardPullFacing3 == 0:
+    cardMeaningNormNum = randint(0,21)
+    cardMeaningNormPull = cardMeaningNorm[cardMeaningNormNum]
+    cardMeaningNormFace = allCardFaces[cardMeaningNormNum]
+    card3 = card(cardMeaningNormPull,cardMeaningNormFace,False)
+    cardImg3 = Image.open("tarot-{cardMeaningNormFace}.jpg")
+    print(card3)
+elif cardPullFacing3 == 1:
+    cardMeaningRevNum = randint(0,21)
+    cardMeaningRevPull = cardMeaningRev[cardMeaningRevNum]
+    cardMeaningRevFace = allCardFaces[cardMeaningRevNum]
+    card3 = card(cardMeaningRevPull,cardMeaningRevFace,True)
+    cardImg3 = Image.open("tarot-{cardMeaningRevFace}.jpg")
+    cardImg3_Rev = cardImg3.rotate(180)
+    cardImg3 = cardImg3_Rev
+    print(card3)
+else:
+    print('error')
+
+results = Image.open("picture.jpg")
+results.paste(cardImg1, (50,100))
+results.paste(cardImg2, (350,100))
+results.paste(cardImg3, (650,100))
+results.save('final.jpg')
+results.show()
